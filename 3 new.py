99fe958ac3c30e387e3fc997586bd380df151202
domibowo <59478422+domibowo@users.noreply.github.com>
2020-01-11 22:59:38 +0700

    Add files via upload

---
+++ 3 new.py
@@ -0,0 +1,7 @@
+input_string = str(input("Hello, "))
+userList = input_string.split()
+print("user list is ", userList)
+def my_function(nol,satu):
+    print("Hello ", nol, satu)
+
+my_function(userList[0], userList[1])
